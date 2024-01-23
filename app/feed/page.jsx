56ecfec2fe93a7df633bee4435ce6456885ba634
Zack Epes <zachary.epes@siemens.com>
2024-01-22 20:19:03 -0500

    added postinstall prisma generate

--- app/feed/page.jsx
+++ app/feed/page.jsx
@@ -26,7 +26,7 @@ export default async function Home() {
       </Button>
 
       <h1>Feed</h1>
-      <div className="grid grid-cols-4 h-full w-full gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 h-full w-full gap-5">
       {posts.map((post) => (
         <Post key={post.id} id={post.id} title={post.title} content={post.content} authorName={post.author.name}/>
       ))}
